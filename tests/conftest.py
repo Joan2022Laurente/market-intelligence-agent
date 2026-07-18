a2a9ed8de26e1c2ce41b97c72e4a4a320dd6d60d
Joan Laurente <joanjcallanaupalaurente@gmail.com>
2026-07-18 12:40:16 -0500

    fix: conftest path para pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import sys
 import os
 
-# Agrega el directorio raíz del proyecto al PYTHONPATH para que 'import src...' funcione
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Agrega la carpeta 'src' al PYTHONPATH para que pytest pueda importar los módulos
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
